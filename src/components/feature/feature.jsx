17f49cac9ddb08d7dd31247f5192e9047c440ae2
Onuh Uche <onuhblaze@gmail.com>
2023-01-05 17:24:20 +0100

    comp

--- src/components/feature/feature.jsx
+++ src/components/feature/feature.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React from 'react'
 import "./feature.css";
 
-const feature = () => {
-  return <div>feature</div>;
-};
+const Feature = () => {
+  return (
+    <div>Feature</div>
+  )
+}
 
-export default feature;
+export default Feature
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./whatGPT3.css";
+
+const Whatgpt3 = () => {
+  return <div>Whatgpt3</div>;
+};
+
+export default Whatgpt3;
