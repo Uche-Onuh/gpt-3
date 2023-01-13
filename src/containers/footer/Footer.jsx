91f33d1d58750a1301bfc953bee676694192716d
Onuh Uche <onuhblaze@gmail.com>
2023-01-13 14:06:15 +0100

    blog

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="gpt3__footer">
+      <div></div>
+    </div>
+  );
 };
 
 export default Footer;
